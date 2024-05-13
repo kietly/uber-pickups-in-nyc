891375c33d06e9d058165d20bf41faa01fe5d57a
kiet.t.ly@gmail.com <kiet.t.ly@gmail.com>
2024-05-13 01:49:48 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import plotly.graph_objects as go
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -7,7 +9,8 @@ from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+    # Set Form Properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    anvil.server.call("get_uber_data")
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,27 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def get_uber_data():
+  df = pd.read_csv(data_files['uber-raw-data-sep14.csv'], nrows=10000)
+  print(df.head())
